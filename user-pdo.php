897412eb7caadd712620b091f8f294d6afcc3ad9
thibaut-matian <thibaut.matian@laplateforme.io>
2025-10-07 14:51:26 +0200

    function connect et disconnect

--- user-pdo.php
+++ user-pdo.php
@@ -49,5 +49,42 @@ class UserPDO {
             'lastname' => $this->lastname
         ];
     }
+   public function connect($login, $password): array {
+        $pdo = $this->pdo();
 
+        $stmt = $pdo->prepare("SELECT id, login, password, email, firstname, lastname FROM utilisateurs WHERE login = ?");
+        $stmt->execute([$login]);
+        $row = $stmt->fetch();
+
+        if (!$row) {
+            return ['error' => 'Login inconnu'];
+        }
+        if (!password_verify($password, $row['password'])) {
+            return ['error' => 'Mot de passe incorrect'];
+        }
+
+        $this->id = (int)$row['id'];
+        $this->login = $row['login'];
+        $this->password = $row['password'];
+        $this->email = $row['email'];
+        $this->firstname = $row['firstname'];
+        $this->lastname = $row['lastname'];
+
+        return [
+            'id' => $this->id,
+            'login' => $this->login,
+            'email' => $this->email,
+            'firstname' => $this->firstname,
+            'lastname' => $this->lastname
+        ];
+    }
+       public function disconnect(): bool {
+        $this->id = null;
+        $this->login = '';
+        $this->password = '';
+        $this->email = '';
+        $this->firstname = '';
+        $this->lastname = '';
+        return true;
+    }
 }
